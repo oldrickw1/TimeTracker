95386652360b0c91e020cb25e1fdc0d7f5fb3ee2
Oldrick Weenink <o.weenink@hotmail.com>
2023-06-12 22:09:29 +0200

    App generates unformatted bar charts with dummy data now

--- timetracker/ActivityEntry.java
+++ timetracker/ActivityEntry.java
@@ -1,4 +1,27 @@
 package com.example.timetracker;
 
 public class ActivityEntry {
+    public ActivityEntry(int time, int day, String activity) {
+        this.time = time;
+        this.day = day;
+        this.activity = activity;
+    }
+
+    private int time;
+    private int day;
+    private String activity;
+
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
 }
--- timetracker/GraphGenerator.java
+++ timetracker/GraphGenerator.java
@@ -2,10 +2,13 @@ package com.example.timetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
@@ -14,13 +17,24 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
-
+import java.util.Arrays;
 
 
 public class GraphGenerator extends AppCompatActivity {
     BarChart mpBarChart;
 
-    private final
+    private final ArrayList<ActivityEntry> dummyEntries = new ArrayList<>(
+            Arrays.asList(
+                    new ActivityEntry(4, 0,"Programming"),
+                    new ActivityEntry(5, 1, "Programming"),
+                    new ActivityEntry(3, 2, "Programming"),
+                    new ActivityEntry(5, 3, "Programming"),
+                    new ActivityEntry(7, 4, "Programming")
+            )
+    );
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +42,21 @@ public class GraphGenerator extends AppCompatActivity {
         setContentView(R.layout.activity_graph_generator);
 
         mpBarChart = findViewById(R.id.barChart);
-        LineDataSet lineDataSet = new LineDataSet(getDataValues(), "Data Set 1");
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(lineDataSet);
+        BarDataSet barDataSet1 = new BarDataSet(getDataValues1(), "Data Set 1");
+        barDataSet1.setBarBorderColor(Color.GREEN);
 
-        LineData data = new LineData(dataSets);
-        mpLineChart.setData(data);
+        BarData barData = new BarData(barDataSet1);
+
+        mpBarChart.setData(barData);
+        mpBarChart.invalidate();
 
     }
 
-    private ArrayList<BarEntry> getDataValues() {
-        ArrayList<Entry> dataVals = new ArrayList<>();
-        dataVals.add(new Entry(0, 20));
-        dataVals.add(new Entry(1, 26));
-        dataVals.add(new Entry(2, 22));
-        dataVals.add(new Entry(3, 2));
-        dataVals.add(new Entry(4, 10));
-
+    private ArrayList<BarEntry> getDataValues1() {
+        ArrayList<BarEntry> dataVals = new ArrayList<>();
+        for (ActivityEntry entry : dummyEntries) {
+            dataVals.add(new BarEntry(entry.getDay(), entry.getTime()));
+        }
         return dataVals;
     }
 }
